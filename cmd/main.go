ebdd784872e1054a6db602b6e9ececb06fc07463
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2025-05-01 20:52:57 +0530

    Send OTP is done

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"knockNSell/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
 
 func main() {
 	router := gin.Default()
--- routes/sendOTP.go
+++ routes/sendOTP.go
@@ -1,19 +1,79 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"crypto/rand"
+	"log"
+	"math/big"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/twilio/twilio-go"
+	openApi "github.com/twilio/twilio-go/rest/api/v2010"
+)
 
 type PhoneModel struct {
-	phoneNumber string
+	PhoneNumber string `json:"phoneNumber" binding:"required"`
+}
+
+func get6DigitNumber() string {
+	max := big.NewInt(900000) // Upper bound (exclusive)
+	n, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		panic(err)
+	}
+	code := n.Int64() + 100000 // Shift to range [100000, 999999]
+	log.Printf("Secure 6-digit code: %d\n", code)
+	return strconv.FormatInt(code, 10)
 }
 
 func Sendotp(c *gin.Context) {
-	var requestBody PhoneModel
-	if error := c.BindJSON(&requestBody); error != nil {
-		c.JSON(401, gin.H{
-			"message": "There was some error",
+	var payload PhoneModel
+
+	if error := c.ShouldBindJSON(&payload); error != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": error.Error(),
+		})
+		return
+	}
+
+	code := get6DigitNumber()
+	messageBody := "Your 6 digit code is: " + code + ". Please do not share it."
+
+	sid := os.Getenv("TWILIO_ACCOUNT_SID")
+	password := os.Getenv("TWILIO_AUTH_TOKEN")
+
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: sid,
+		Password: password,
+	})
+
+	params := &openApi.CreateMessageParams{}
+	countryCode := "+91"
+	params.SetFrom("+19786984267")                  // Your Twilio phone number
+	params.SetTo(countryCode + payload.PhoneNumber) // Recipient's phone number
+	params.SetBody(messageBody)
+
+	resp, error := client.Api.CreateMessage(params)
+	if error != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": error.Error(),
+		})
+		return
+	} else {
+		if resp.Sid != nil {
+			log.Println(*resp.Sid)
+		} else {
+			log.Println(*resp.Sid)
+		}
+		c.JSON(200, gin.H{
+			"status":       resp.Status,
+			"date_created": resp.DateCreated,
+			"phone":        resp.To,
+			"date_updated": resp.DateUpdated,
+			"message":      "OTP send successfully",
 		})
 	}
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
+
 }
